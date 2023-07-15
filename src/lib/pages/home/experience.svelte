893b2bfb69682e7752c8ed6a7210524943744e6a
Kyle Szalai <kyleszalai@gmail.com>
2023-07-15 15:32:16 -0400

    Adding text-center to company container

--- src/lib/pages/home/experience.svelte
+++ src/lib/pages/home/experience.svelte
@@ -13,7 +13,7 @@
 <div class="max-w-7xl h-screen-adjusted mx-auto border-red-600 border-2 flex justify-center items-center">
     <div class="w-48 mx-4 flex flex-col justify-start">
         {#each experience.jobs as job}
-            <div class="w-48 py-2 font-mono bg-blue-300 hover:bg-blue-500 transition-colors" on:click={() => { selectExperience(job) }}>
+            <div class="w-48 p-2 font-mono text-center bg-blue-300 hover:bg-blue-500 transition-colors" on:click={() => { selectExperience(job) }}>
                 {job.company}
             </div>
         {/each}
